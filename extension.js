27fdad285e6ef35b3795e911db41ecbbec84ac43
JangHo Seo <jangho@jangho.kr>
2016-10-09 10:48:54 +0900

    hide existing actions and add logout action

--- extension.js
+++ extension.js
@@ -1,53 +1,31 @@
-
-const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const Lang = imports.lang;
+const system = Main.panel.statusArea['aggregateMenu']._system;
+const actor = system._actionsItem.actor;
 
-let text, button;
+let logOutAction;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
+function _onLogOutClicked() {
+  this.menu.itemActivated();
+  Main.overview.hide();
+  this._session.LogoutRemote(0);
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+  logOutAction = system._createActionButton('system-log-out-symbolic', _("Log Out"));
+  logOutAction.connect('clicked', Lang.bind(system, _onLogOutClicked));
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+  actor.add(logOutAction, { expand: true, x_fill: false });
+  actor.remove_actor(system._settingsAction);
+  actor.remove_actor(system._lockScreenAction);
+  actor.remove_actor(system._altSwitcher.actor);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+  actor.remove_actor(logOutAction);
+  actor.add(system._settingsAction, { expand: true, x_fill: false });
+  actor.add(system._lockScreenAction, { expand: true, x_fill: false });
+  actor.add(system._altSwitcher.actor, { expand: true, x_fill: false });
 }
